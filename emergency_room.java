4ec20513975c108971efca481074dc004e3fd12f
JoeLancey <j.odtohan.557940@umindanao.edu.ph>
2025-09-29 16:08:44 +0800

    Update emergency_room.java

--- emergency_room.java
+++ emergency_room.java
@@ -5,39 +5,37 @@ import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 
-class EmergencyRoomQueue {
-    private PriorityQueue<Patient> queue;
+public class emergency_room {
+    private PriorityQueue<patients> queue;
 
-    public EmergencyRoomQueue() {
+    public emergency_room() {
         queue = new PriorityQueue<>();
     }
 
     public void addPatient(String name, int prio, String condition, String time) {
-        Patient patient = new Patient(name, prio, condition, time);
+        patients patient = new patients(name, prio, condition, time);
         queue.add(patient);
     }
 
     public void treatNextPatient() {
         if (queue.isEmpty()) {
-            System.out.println("NO PATIENTS ARE WAITING	");
+            System.out.println("NO PATIENTS ARE WAITING");
         } else {
-            Patient next = queue.poll();
-            System.out.println("\nTreating patient");
+            patients next = queue.poll();
             System.out.println("Treated: " + next);
         }
     }
 
     public void displayQueue() {
         System.out.println("Patients Waiting: " + queue.size());
-
-        List<Patient> patients = new ArrayList<>(queue);
-        Collections.sort(patients);
-
-        int index = 1;
         
-        for (Patient p : patients) {
-            System.out.println(index + ". " + p);
-            index++;
+        List<patients> list = new ArrayList<>(queue);
+        Collections.sort(list);
+
+        int idx = 1;
+        for (patients p : list) {
+            System.out.println(idx + ". " + p);
+            idx++;
         }
     }
 }
